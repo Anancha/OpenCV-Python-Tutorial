77d160cf45f3af5de118ef9e2f237b4fa061350b
Anucha Ananbenjapon <anucha_a@hotmail.com>
2021-07-08 10:57:16 +0700

    Commit

--- ch04-图片/4.imread_imshow_imwrite.py
+++ ch04-图片/4.imread_imshow_imwrite.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('data/messi5.jpg', 0)   
+img = cv2.imread('data/messi5.jpg', 0)   #Edit path from missi5.jpg to data/missi5.jpg
 cv2.imshow('image', img)
 
 k = cv2.waitKey(0)
